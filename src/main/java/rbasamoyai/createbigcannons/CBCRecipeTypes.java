8fcb35e78f4de159224f61e6a0ee0f184346a090
Steveplays28 <djspaargaren@outlook.com>
2023-01-30 19:44:13 +0100

    refactor: Fix `CBCRecipeTypes`

--- src/main/java/rbasamoyai/createbigcannons/CBCRecipeTypes.java
+++ src/main/java/rbasamoyai/createbigcannons/CBCRecipeTypes.java
@@ -1,37 +1,30 @@
 package rbasamoyai.createbigcannons;
 
-import java.util.function.Supplier;
-
-import javax.annotation.Nullable;
-
 import com.simibubi.create.AllRecipeTypes;
 import com.simibubi.create.content.contraptions.processing.ProcessingRecipeBuilder.ProcessingRecipeFactory;
 import com.simibubi.create.content.contraptions.processing.ProcessingRecipeSerializer;
 import com.simibubi.create.foundation.utility.Lang;
 import com.simibubi.create.foundation.utility.recipe.IRecipeTypeInfo;
-
+import io.github.fabricators_of_create.porting_lib.util.LazyRegistrar;
+import io.github.fabricators_of_create.porting_lib.util.RegistryObject;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.crafting.SimpleRecipeSerializer;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import rbasamoyai.createbigcannons.crafting.foundry.MeltingRecipe;
 import rbasamoyai.createbigcannons.crafting.item_munitions.CartridgeAssemblyRecipe;
 import rbasamoyai.createbigcannons.crafting.item_munitions.MunitionFuzingRecipe;
 
+import java.util.function.Supplier;
+
 public enum CBCRecipeTypes implements IRecipeTypeInfo {
 
-	MELTING(MeltingRecipe::new),
-	MUNITION_FUZING(() -> new SimpleRecipeSerializer<>(MunitionFuzingRecipe::new), () -> RecipeType.CRAFTING, false),
-	CARTRIDGE_ASSEMBLY(() -> new SimpleRecipeSerializer<>(CartridgeAssemblyRecipe::new), () -> RecipeType.CRAFTING, false);
+	MELTING(MeltingRecipe::new), MUNITION_FUZING(() -> new SimpleRecipeSerializer<>(MunitionFuzingRecipe::new), () -> RecipeType.CRAFTING, false), CARTRIDGE_ASSEMBLY(() -> new SimpleRecipeSerializer<>(CartridgeAssemblyRecipe::new), () -> RecipeType.CRAFTING, false);
 
 	private final ResourceLocation id;
-	private final RegistryObject<RecipeSerializer<?>> serializer;
-	@Nullable private final RegistryObject<RecipeType<?>> typeObj;
+	private final io.github.fabricators_of_create.porting_lib.util.RegistryObject<RecipeSerializer<?>> serializer;
+	private final RegistryObject<RecipeType<?>> typeObj;
 	private final Supplier<RecipeType<?>> type;
 
 	CBCRecipeTypes(Supplier<RecipeSerializer<?>> serializerSupplier, Supplier<RecipeType<?>> typeSupplier, boolean registerType) {
@@ -46,7 +39,7 @@ public enum CBCRecipeTypes implements IRecipeTypeInfo {
 			this.type = typeSupplier;
 		}
 	}
-	
+
 	CBCRecipeTypes(Supplier<RecipeSerializer<?>> serializer) {
 		String name = Lang.asId(this.name());
 		this.id = CreateBigCannons.resource(name);
@@ -54,33 +47,36 @@ public enum CBCRecipeTypes implements IRecipeTypeInfo {
 		this.typeObj = Registries.RECIPE_TYPES.register(name, () -> AllRecipeTypes.simpleType(this.id));
 		this.type = this.typeObj;
 	}
-	
+
 	CBCRecipeTypes(ProcessingRecipeFactory<?> factory) {
 		this(() -> new ProcessingRecipeSerializer<>(factory));
 	}
-	
-	@Override public ResourceLocation getId() { return this.id; }
-	
+
+	public static void register() {
+		Registries.SERIALIZERS.register();
+		Registries.RECIPE_TYPES.register();
+	}
+
+	@Override
+	public ResourceLocation getId() {
+		return this.id;
+	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T extends RecipeSerializer<?>> T getSerializer() {
 		return (T) this.serializer.get();
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T extends RecipeType<?>> T getType() {
 		return (T) this.type.get();
 	}
-	
-	public static void register(IEventBus modEventBus) {
-		Registries.SERIALIZERS.register(modEventBus);
-		Registries.RECIPE_TYPES.register(modEventBus);
-	}
-	
+
 	private static class Registries {
-		private static final DeferredRegister<RecipeSerializer<?>> SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, CreateBigCannons.MOD_ID);
-		private static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(Registry.RECIPE_TYPE_REGISTRY, CreateBigCannons.MOD_ID);
+		private static final LazyRegistrar<RecipeSerializer<?>> SERIALIZERS = LazyRegistrar.create(Registry.RECIPE_SERIALIZER, CreateBigCannons.MOD_ID);
+		private static final LazyRegistrar<RecipeType<?>> RECIPE_TYPES = LazyRegistrar.create(Registry.RECIPE_TYPE_REGISTRY, CreateBigCannons.MOD_ID);
 	}
 
 }
