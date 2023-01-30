95de244ac3ab2fe7addae13fda474dd054418e13
Steveplays28 <djspaargaren@outlook.com>
2023-01-30 22:31:45 +0100

    refactor: Fix `Grapeshot`

--- src/main/java/rbasamoyai/createbigcannons/munitions/big_cannon/grapeshot/Grapeshot.java
+++ src/main/java/rbasamoyai/createbigcannons/munitions/big_cannon/grapeshot/Grapeshot.java
@@ -5,23 +5,26 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import rbasamoyai.createbigcannons.CBCTags;
-import rbasamoyai.createbigcannons.CreateBigCannons;
 import rbasamoyai.createbigcannons.config.CBCConfigs;
 import rbasamoyai.createbigcannons.munitions.big_cannon.shrapnel.Shrapnel;
 
 public class Grapeshot extends Shrapnel {
 
+	private static final DamageSource GRAPESHOT = DamageSource.MAGIC;
+
 	public Grapeshot(EntityType<? extends Grapeshot> type, Level level) {
 		super(type, level);
 	}
-	
+
 	@Override
 	protected boolean canDestroyBlock(BlockState state) {
 		return state.is(CBCTags.BlockCBC.GRAPESHOT_SHATTERABLE)
-			|| state.is(CBCTags.BlockCBC.GRAPESHOT_VULNERABLE) && this.random.nextFloat() < CBCConfigs.SERVER.munitions.grapeshotVulnerableBreakChance.getF();
+				|| state.is(CBCTags.BlockCBC.GRAPESHOT_VULNERABLE) && this.random.nextFloat() < CBCConfigs.SERVER.munitions.grapeshotVulnerableBreakChance.getF();
 	}
-	
-	private static final DamageSource GRAPESHOT = new DamageSource(CreateBigCannons.MOD_ID + ".grapeshot");
-	@Override protected DamageSource getDamageSource() { return GRAPESHOT; }
+
+	@Override
+	protected DamageSource getDamageSource() {
+		return GRAPESHOT;
+	}
 
 }
